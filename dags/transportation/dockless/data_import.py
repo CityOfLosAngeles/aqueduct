7c1bf421ecf2d9c3fcf3ece3fbbe3da18da6e697
Tim Black <timothy.c.black@gmail.com>
2018-10-09 12:52:25 -0700

    provider class implmentation

--- dags/transportation/dockless/data_import.py
+++ dags/transportation/dockless/data_import.py
@@ -3,12 +3,99 @@ import yaml
 import os
 import json
 import datetime, time, pytz
-# Config files
-from config import config as cfg
-# AWS
-# create a new bucket
+# AWS: Create bucket
 
-def get_provider_data(provider, feed, start_time=None, end_time=None, testing=True, **kwargs):
+# Load config file
+with open('config.yml', 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+class MDSProviderApi: 
+    """ Class representing an MDS provider API """
+    def __init__(self, name):
+        self.name = self.set_name(name)
+        self.baseurl = self.set_url()
+        self.token = self.set_token()
+        self.headers = self.compose_header()
+        self.paginate = False #hmm
+
+    def set_name(self, name):
+        name = name.lower()
+        if name not in cfg['provider'].keys():
+            raise KeyError("Provider {} not in list of providers.".format(name))
+        return name
+
+    def set_url(self):
+        if 'baseurl' not in cfg['provider'][self.name].keys():
+            raise KeyError("No base url defined for provider {}.".format(self.name))
+        baseurl = cfg['provider'][self.name]['baseurl']
+        return baseurl
+
+    def set_token(self):
+        if 'token' not in cfg['provider'][self.name].keys():
+            raise KeyError("No token defined for provider {}.".format(self.name))
+        token = cfg['provider'][self.name]['token']
+        return token
+
+    def compose_header(self):
+        if self.name == 'bird':
+            auth = 'Bird ' + self.token
+            header = {'Authorization': auth, 'APP-Version': '3.0.0'}
+        else:
+            auth = 'Bearer ' + self.token
+            header = {'Authorization': auth}
+        return header
+    
+    def get_data(self, feed, testing=True, **kwargs):
+
+        # Set url, params
+        if feed == 'trips':
+            url = self.baseurl + '/trips'
+        elif feed == 'status_changes':
+            url = self.baseurl + '/status_changes'
+        else:
+            print('Not a valid feed')
+            return None
+        params = kwargs
+
+        # Initial request
+        r = requests.get(url, headers = self.headers, params = params)
+        if r.status_code != requests.codes.ok:
+            print(r.status_code)
+            return None
+        first_page = r.json()
+        provider_data = first_page['data']
+        if 'links' not in first_page.keys():
+            return provider_data
+        
+        # Paginate, if applicable
+        if testing == True:
+            i = 0
+            next_url = first_page['links']['next']
+            while i < 3:
+                r = requests.get(next_url, headers = headers, params = params)
+                if r.status_code != requests.codes.ok:
+                    print(r.status_code)
+                    return None
+                next_page = r.json()
+                next_url = next_page['links']['next']
+                for record in next_page['data']:
+                    provider_data.append(record)
+                i += 1
+        # Paginate, if applicable
+        if testing == False:
+            next_url = first_page['links']['next']
+            while next_url is not None:
+                r = requests.get(next_url, headers = headers, params = params)
+                if r.status_code != requests.codes.ok:
+                    print(r.status_code)
+                    return None
+                next_page = r.json()
+                next_url = next_page['links']['next']
+                for record in next_page['data']:
+                    provider_data.append(record)
+        return provider_data
+
+def get_provider_data(provider_name, feed, start_time=None, end_time=None, testing=True, **kwargs):
     """ Query provider API
 
     Args:
@@ -23,50 +110,14 @@ def get_provider_data(provider, feed, start_time=None, end_time=None, testing=Tr
 
     """
     # Set params
-    url = cfg.provider[provider][feed]
-    headers = {'Authorization': cfg.provider[provider]['auth']}
+    provider = MDSProviderApi(provider_name)
     params = kwargs
     for lbl, timeval in {'start_time': start_time, 'end_time': end_time}.items():
         if timeval is not None:
             unix_secs = time.mktime(timeval.timetuple())
             params[lbl] = unix_secs
+    provider_data = provider.get_data(feed, params)
     
-    # Initial GET reuest
-    r = requests.get(url, headers = headers, params = params)
-    if r.status_code != requests.codes.ok:
-        print(r.status_code)
-        return None
-    first_page = r.json()
-    provider_data = first_page['data']
-
-    # Paginate through results
-    if testing == True:
-        i = 0
-        next_url = first_page['links']['next']
-        while i < 3:
-            r = requests.get(next_url, headers = headers, params = params)
-            if r.status_code != requests.codes.ok:
-                print(r.status_code)
-                return None
-            next_page = r.json()
-            next_url = next_page['links']['next']
-            for record in next_page['data']:
-                provider_data.append(record)
-            i += 1
-    
-    # Paginate through results
-    if testing == False:
-        next_url = first_page['links']['next']
-        while next_url is not None:
-            r = requests.get(next_url, headers = headers, params = params)
-            if r.status_code != requests.codes.ok:
-                print(r.status_code)
-                return None
-            next_page = r.json()
-            next_url = next_page['links']['next']
-            for record in next_page['data']:
-                provider_data.append(record)
-        
     # TODO: Dump data to S3 bucket
     # Format filename by time quey params
     start_str = "{:04}{:02}{:02}{:02}{:02}".format(*start_time.timetuple()[0:5])
