e303f69bac055db5b9e407e3d16e86b1db348d4e
Lei Cao <leicao.me@gmail.com>
2018-10-10 16:49:24 -0700

    add graph to email content

--- dags/myla311/dag_311_outlier_detector.py
+++ dags/myla311/dag_311_outlier_detector.py
@@ -30,7 +30,6 @@
 #     email_cc = ['address3', 'address4',..]
 
 import os
-
 import logging
 import airflow
 import smtplib
@@ -41,13 +40,19 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.postgres_operator import PostgresOperator
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 from config import *
+import matplotlib.pyplot as plt
+from matplotlib import *
+import seaborn as sns
+import altair as alt
 
 # a csv file with this filename will be saved from a copy of postgres table
-filename = "./myla311-postgres.csv"
+filename = "./myla311.csv"
 
 # email parameters
-message = "This last week's Outliers:\n\n"
+message = "This last week's Outliers:"
 email_to = ', '.join(email_to) # should be setup as environment variable, e.g. email_to = ['address1', 'address2']
 email_cc = ', '.join(email_cc) # same as above
 subject = '[Alert] MyLA311 Data Outliers'
@@ -62,7 +67,34 @@ DIFF_LOW_OUTLIER = 'LOW TOTAL DIFF OUTLIER'
 HIGH_PROCESS_TIME_OUTLIER = 'HIGH PROCESS TIME OUTLIER'
 LOW_PROCESS_TIME_OUTLIER = 'LOW PROCESS TIME OUTLIER'
 
-def detect_outliers(**kwargs):
+
+def make_save_graph(df, col, title):
+    line = alt.Chart(df.reset_index(), title = ' - '.join([title, col])).mark_line().encode(
+    x='year_week:O',
+    y= col+':Q')
+    rule = alt.Chart(df).mark_rule(color='red').encode(
+    alt.Y('average({})'.format(col)),
+    size=alt.value(1))
+    graph = line + rule
+    filename = 'chart-{}.png'.format(col.replace('/', '-').replace(' ','-'))
+    graph.save(filename)
+    return filename
+
+
+def make_save_boxplot(df,point,title):
+    # using seaborn
+    sns.set_style("whitegrid")
+    fig, ax = plt.subplots(figsize=(8,8))
+    ax.set_title(title, fontsize=15)
+    plot1 = sns.boxplot(ax=ax, x=df, linewidth=1, color='lightblue')
+    plot2 = plt.scatter(point, 0, marker='o', s=100, c='red', linewidths=5,label='Outlier')
+    ax.legend()
+    filename = 'chart-Proc-Time-{}.png'.format(title.replace('/', '-').replace(' ','-'))
+    plt.savefig(filename)
+    return filename
+
+
+def detect_outliers(filename, **kwargs):
 
     """
     Outlier Detector that detects the following types of outliers:
@@ -117,24 +149,37 @@ def detect_outliers(**kwargs):
     df_last_week = df_last_whole_yr.loc[last_yw]
 
     cols = df_last_whole_yr.columns
-
+    cols = cols.drop('Total')
     # make the alert dict
     alert = {}
 
     # Get individual requet_type outliers
     for x in cols:
+        flag = False
         if df_last_week[x] > df_last_whole_yr[x].mean() + 2 * df_last_whole_yr[x].std():
-            alert[x] = INDIV_HIGH_OUTLIER
+            alert[x] = [INDIV_HIGH_OUTLIER]
+            flag = True
         elif df_last_week[x] < df_last_whole_yr[x].mean() - 2 * df_last_whole_yr[x].std():
-            alert[x] = INDIV_LOW_OUTLIER
+            alert[x] = [INDIV_LOW_OUTLIER]    
+            flag = True
+        if flag:
+            filename = make_save_graph(df_last_whole_yr, x, 'Indiv Req Type Outlier')
+            alert[x].append(filename)
 
     # Get weekly total outliers
-    high_bound = df_last_whole_yr['Total'].mean() + 2 * df_last_whole_yr['Total'].std()
-    low_bound = df_last_whole_yr['Total'].mean() - 2 * df_last_whole_yr['Total'].std()
-    if df_last_week['Total'] > high_bound:
-        alert['Total High'] = TOTAL_HIGH_OUTLIER
-    elif df_last_week['Total'] < low_bound:
-        alert['Total Low'] = TOTAL_LOW_OUTLIER
+    col = 'Total'
+    high_bound = df_last_whole_yr[col].mean() + 2 * df_last_whole_yr[col].std()
+    low_bound = df_last_whole_yr[col].mean() - 2 * df_last_whole_yr[col].std()
+    flag = False
+    if df_last_week[col] > high_bound:
+        alert[col] = [TOTAL_HIGH_OUTLIER]
+        flag = True
+    elif df_last_week[col] < low_bound:
+        alert[col] = [TOTAL_LOW_OUTLIER]
+        flag = True
+    if flag:
+        filename = make_save_graph(df_last_whole_yr, col, col+' Outlier')
+        alert[col].append(filename)
 
     # Get Diff of weekly total outliers
     pd.options.mode.chained_assignment = None  # turn off Value to be set on a copy of a slice warning
@@ -142,15 +187,22 @@ def detect_outliers(**kwargs):
     df_last_whole_yr['Diff'] = df_last_whole_yr['Diff'].abs()
     pd.options.mode.chained_assignment = "warn"
 
-    high_bound = df_last_whole_yr['Diff'].mean() + 2 * df_last_whole_yr['Diff'].std()
-    low_bound = df_last_whole_yr['Diff'].mean() - 2 * df_last_whole_yr['Diff'].std()
-    if df_last_whole_yr['Diff'][last_yw] > high_bound:
-        alert['Diff High'] = DIFF_HIGH_OUTLIER
-    elif df_last_whole_yr['Diff'][last_yw] < low_bound:
-        alert['Diff Low'] = DIFF_LOW_OUTLIER
+    col = 'Diff'
+    high_bound = df_last_whole_yr[col].mean() + 2 * df_last_whole_yr[col].std()
+    low_bound = df_last_whole_yr[col].mean() - 2 * df_last_whole_yr[col].std()
+    flag = False
+    if df_last_whole_yr[col][last_yw] > high_bound:
+        alert[col] = [DIFF_HIGH_OUTLIER]
+        flag = True
+    elif df_last_whole_yr[col][last_yw] < low_bound:
+        alert[col] = [DIFF_LOW_OUTLIER]
+        flag = True
+    if flag:
+        filename = make_save_graph(df_last_whole_yr, col, 'Weekly Diff Outlier')
+        alert[col].append(filename)
 
     # Get process time outliers
-    df['process_time'] = (df['closed_datetime'] - df['created_datetime']).apply(lambda x: x.total_seconds())
+    df['process_time'] = (df['closed_datetime'] - df['created_datetime']).apply(lambda x: x.total_seconds()/3600)
     df_process_time = df[['srnumber', 'created_date', 'request_type', 'process_time']]
 
     df_process_time.dropna(inplace=True)
@@ -168,50 +220,119 @@ def detect_outliers(**kwargs):
     df_process_time = df_process_time[first_day_of_last_yr_week : last_week_end]
     df_proc_time_last_week = df_process_time.loc[last_week_start : last_week_end]
 
-    mean = df_process_time['process_time'].mean()
-    std = df_process_time['process_time'].std()
-    high_bound = mean + 2 * std
-    low_bound = mean - 2 * std
-    for index, row in df_proc_time_last_week.iterrows():
-        if row['process_time'] > high_bound:
-            alert['srnumber: ' + row['srnumber']] = HIGH_PROCESS_TIME_OUTLIER + ", " + row['request_type'] + ", " \
-                                                    + str(int(round(row['process_time'] / 3600, 1))) + " hrs"
-        elif row['process_time'] < low_bound:
-            alert['srnumber: ' + row['srnumber']] = LOW_PROCESS_TIME_OUTLIER + ", " + row['request_type'] + ", " \
-                                                    + str(int(round(row['process_time'] / 3600, 1))) + " hrs"
+    for req_type in cols:
+        df_temp = df_process_time[df_process_time.request_type == req_type]
+        mean = df_temp['process_time'].mean()
+        std = df_temp['process_time'].std()
+        high_bound = mean + 2 * std
+        low_bound = mean - 2 * std
+        df_temp1 = df_proc_time_last_week[df_proc_time_last_week.request_type == req_type]
+        for index, row in df_temp1.iterrows():
+            flag = False
+           
+            if row['process_time'] > high_bound:
+                alert['srnumber: ' + row['srnumber']] = [HIGH_PROCESS_TIME_OUTLIER + ", " + row['request_type'] + ", "                                       + str(row['process_time']) +" hrs"]
+                flag = True
+            elif row['process_time'] < low_bound:
+                alert['srnumber: ' + row['srnumber']] = [LOW_PROCESS_TIME_OUTLIER + ", " + row['request_type'] + ", "                                       + str(row['process_time']) +" hrs"]
+                flag = True
+            if flag:
+                filename = make_save_boxplot(df_temp['process_time'], row['process_time'], req_type + ' SRNUMBER:' + row['srnumber'])
+                alert['srnumber: ' + row['srnumber']].append(filename)
+
     logging.info('Alert: ' + str(alert))
     return alert
+
 
 def sendemail(from_addr, to_addr_list, cc_addr_list, subject,
               message, login, password, smtpserver, **kwargs):
     # xcom_pull alert from last task's return value
     task_instance = kwargs['task_instance']
     alert = task_instance.xcom_pull(task_ids='detect_outliers')
+
     # if no outliers found, skip sending email
     if bool(alert):
         # prepare email body
         logging.info(alert)
-        count = 1
+
+        #************************************************************************
+        msgRoot = MIMEMultipart('related')
+        msgRoot['Subject'] = subject
+        msgRoot['From'] = from_addr
+        msgRoot['To'] = to_addr_list
+        msgRoot.preamble = 'This is a multi-part message in MIME format.'
+
+        # Encapsulate the plain and HTML versions of the message body in an
+        # 'alternative' part, so message agents can decide which they want to display.
+        msgAlternative = MIMEMultipart('alternative')
+        msgRoot.attach(msgAlternative)
+
+        msgText = MIMEText('Please review this email on a computer in order to read HTML content')
+        msgAlternative.attach(msgText)
+
+        # We reference the image in the IMG SRC attribute by the ID we give it below
+        html_content = make_html_content(alert, message)
+
+        msgText1 = MIMEText(html_content, 'html')
+        msgAlternative.attach(msgText1)
+        
+        # This example assumes the image is in the current directory
+        count=1
         for key, value in alert.items():
-            # print(key, value)
-            message += str(count) + ". " + key + ": " + value + "\n"
-            # print(message)
+            fp = open(value[1], 'rb')
+            msgImage = MIMEImage(fp.read())
+            fp.close()
+            msgImage.add_header('Content-ID', '<image'+ str(count) +'>')
+            msgRoot.attach(msgImage)
             count += 1
-
-        msg = MIMEMultipart()
-        msg['From'], msg['To'], msg['Cc'], msg['Subject'] = from_addr, to_addr_list, cc_addr_list, subject # specify your sender, receiver, subject attributes
-
-        body = MIMEText(message, 'plain')  # convert the body to a MIME compatible string
-        msg.attach(body)
-
+        #************************************************************************
         server = smtplib.SMTP(smtpserver)
         server.starttls()
         server.login(login, password)
-        server.sendmail(msg['From'], [to_addr_list, cc_addr_list], msg.as_string())
+        server.sendmail(from_addr, [to_addr_list, cc_addr_list], msgRoot.as_string())
+        server.quit()
         return "Outliers founded. Alert Email sent. Success!"
 
     return "No alert generated. Email not sent."
 
+def make_html_content(alert, message):
+    count = 1
+    c = message.join(['<br><b>', '</b></br><br />'])
+    for key, value in alert.items():
+        c += str(count) + '. ' + value[0] + ': ' + key
+        c += '<br><img src="cid:image' + str(count) + '"><br>'
+        count += 1
+    return c
+
+
+def remove_graph_png(**kwargs):
+    task_instance = kwargs['task_instance']
+    alert = task_instance.xcom_pull(task_ids='detect_outliers')
+    alert = {'Illegal Dumping Pickup': ['HIGH INDIVIDUAL REQUEST TYPE OUTLIER',
+          'chart-Illegal-Dumping-Pickup.png'],
+         'Illegal Dumping in Progress': ['HIGH INDIVIDUAL REQUEST TYPE OUTLIER',
+          'chart-Illegal-Dumping-in-Progress.png'],
+         'Median Island Maintenance': ['HIGH INDIVIDUAL REQUEST TYPE OUTLIER',
+          'chart-Median-Island-Maintenance.png'],
+         'Park Fields/Buildings/Repairs': ['HIGH INDIVIDUAL REQUEST TYPE OUTLIER',
+          'chart-Park-Fields-Buildings-Repairs.png'],
+         'Park Graffiti/Trash': ['HIGH INDIVIDUAL REQUEST TYPE OUTLIER',
+          'chart-Park-Graffiti-Trash.png'],
+         'Park Homelessness and Security': ['HIGH INDIVIDUAL REQUEST TYPE OUTLIER',
+          'chart-Park-Homelessness-and-Security.png'],
+         'Tables and Chairs Obstructing': ['HIGH INDIVIDUAL REQUEST TYPE OUTLIER',
+          'chart-Tables-and-Chairs-Obstructing.png'],
+         'srnumber: 1-1197288301': ['HIGH PROCESS TIME OUTLIER, Dead Animal Removal, 44.53527777777778 hrs',
+          'chart-Proc-Time-Dead-Animal-Removal-SRNUMBER:1-1197288301.png'],
+         'srnumber: 1-1197458141': ['HIGH PROCESS TIME OUTLIER, Dead Animal Removal, 46.02111111111111 hrs',
+          'chart-Proc-Time-Dead-Animal-Removal-SRNUMBER:1-1197458141.png']}
+    
+    for key, value in alert.items():
+        file = value[1]
+        if os.path.exists(file):
+          os.remove(file)
+        else:
+          print("The file "+ file + " does not exist. Skipping to the next graph png")
 
 sql_pull_data = \
     """
@@ -242,6 +363,7 @@ task0 = PostgresOperator(
 task1 = PythonOperator(
     task_id='detect_outliers',
     provide_context=True,
+    op_args=[filename],
     python_callable=detect_outliers,
     dag=dag)
 
@@ -254,4 +376,10 @@ task2 = PythonOperator(
     python_callable=sendemail,
     dag=dag)
 
-task0 >> task1 >> task2
+task3 = PythonOperator(
+    task_id='remove_graph_png',
+    provide_context=True,
+    python_callable=remove_graph_png,
+    dag=dag)
+
+task0 >> task1 >> task2 >> task3
