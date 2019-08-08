8c7f13e4f6bbdf0bfa469af016a25d8100d0067a
Hunter Owens <owens.hunter@gmail.com>
2019-08-08 14:44:50 -0700

    partially working geom parse

--- dags/transportation/dockless/geom_parse.sql
+++ dags/transportation/dockless/geom_parse.sql
@@ -28,7 +28,42 @@ END;
 $BODY$
 language plpgsql;
 /* trigger itself */ 
-CREATE TRIGGER route_process
+CREATE OR REPLACE TRIGGER route_process
+     AFTER INSERT ON trips
+     FOR EACH ROW
+     EXECUTE PROCEDURE geom_copy();
+
+/* create a status_change_geoms table */ 
+CREATE TABLE IF NOT EXISTS status_change_geoms (
+	status_change_id UUID NOT NULL,
+	points GEOMETRY 
+); 
+/* process route function */ 
+CREATE OR REPLACE FUNCTION process_status_change(jsonb) 
+	RETURNS geometry 
+	LANGUAGE 'sql'
+	AS $BODY$
+
+	SELECT  
+	ST_MakeLine(ARRAY(
+        SELECT ST_GeomFromGeoJSON((feature -> 'geometry')::text) as geometry FROM (
+            SELECT jsonb_array_elements($1 -> 'features') as feature
+        ) features
+    ))
+$BODY$;
+/* trigger function */ 
+CREATE OR REPLACE FUNCTION geom_copy() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    INSERT INTO
+        trips_geoms(trip_id,points)
+        VALUES(new.trip_id,process_route(new.route));
+           RETURN new;
+END;
+$BODY$
+language plpgsql;
+/* trigger itself */ 
+CREATE OR REPLACE TRIGGER route_process
      AFTER INSERT ON trips
      FOR EACH ROW
      EXECUTE PROCEDURE geom_copy();
