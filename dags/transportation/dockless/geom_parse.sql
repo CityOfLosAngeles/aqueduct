ff45489d3f8732fbad43c0efadc1f5e68d6522e4
Hunter Owens <owens.hunter@gmail.com>
2019-08-12 13:17:08 -0700

    dockless status_change geom processor

--- dags/transportation/dockless/geom_parse.sql
+++ dags/transportation/dockless/geom_parse.sql
@@ -38,35 +38,35 @@ CREATE OR REPLACE TRIGGER route_process
 
 /* create a status_change_geoms table */ 
 CREATE TABLE IF NOT EXISTS status_change_geoms (
-	status_change_id UUID NOT NULL,
-	points GEOMETRY 
+	status_change_id INTEGER NOT NULL REFERENCES status_changes(id),
+	event_location_geom GEOMETRY 
 ); 
-/* process route function */ 
-CREATE OR REPLACE FUNCTION process_status_change(jsonb) 
-	RETURNS geometry 
-	LANGUAGE 'sql'
-	AS $BODY$
+/* process location function function */ 
+-- jsonb input
+CREATE OR REPLACE FUNCTION process_status_change_location(jsonb)
+    	RETURNS geometry AS 
+	$func$
+	BEGIN
+		RETURN st_setsrid(st_geomfromgeojson(($1 -> 'geometry'::text)::text), 4326);
+	EXCEPTION WHEN OTHERS THEN
+		return NULL;
+	END
+	$func$  
+    LANGUAGE plpgsql IMMUTABLE;
 
-	SELECT  
-	ST_MakeLine(ARRAY(
-        SELECT ST_GeomFromGeoJSON((feature -> 'geometry')::text) as geometry FROM (
-            SELECT jsonb_array_elements($1 -> 'features') as feature
-        ) features
-    ))
-$BODY$;
 /* trigger function */ 
-CREATE OR REPLACE FUNCTION geom_copy() RETURNS TRIGGER AS
+CREATE OR REPLACE FUNCTION status_geom_copy() RETURNS TRIGGER AS
 $BODY$
 BEGIN
     INSERT INTO
-        trips_geoms(trip_id,points)
-        VALUES(new.trip_id,process_route(new.route));
+        status_change_geoms(status_change_id, event_location_geom)
+        VALUES(new.id,process_status_change_location(new.event_location));
            RETURN new;
 END;
 $BODY$
 language plpgsql;
 /* trigger itself */ 
-CREATE OR REPLACE TRIGGER route_process
-     AFTER INSERT ON trips
+CREATE OR REPLACE TRIGGER status_change_processor
+     AFTER INSERT ON status_changes
      FOR EACH ROW
-     EXECUTE PROCEDURE geom_copy();
+     EXECUTE PROCEDURE status_geom_copy();
