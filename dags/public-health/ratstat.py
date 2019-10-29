3a9193903cf7eb902e1f421e2c80a229a1bd7840
Hunter Owens <owens.hunter@gmail.com>
2019-10-29 13:06:12 -0700

    fix broken datetime import

--- dags/public-health/ratstat.py
+++ dags/public-health/ratstat.py
@@ -3,6 +3,8 @@ import os
 from sqlalchemy import create_engine
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
 
 def ratstat_loader():
     """
