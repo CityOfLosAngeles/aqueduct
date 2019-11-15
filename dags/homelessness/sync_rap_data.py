789e891ba1aa1b11514d96621ab45dcaa96ae5c5
Brendan Bailey <myrtmp191@cds.ci.la.ca.us>
2019-11-15 11:12:04 -0800

    Edits to sync_rap_data.py. Used connections for authentication, set task order, add Hunter and Brendan to email list, and wrote files from FTP to tmp folder

--- dags/homelessness/sync_rap_data.py
+++ dags/homelessness/sync_rap_data.py
@@ -7,32 +7,35 @@ import arcgis
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.base_hook import BaseHook
 from datetime import datetime, timedelta
 
 def get_file(ftphost, ftpuser, ftppassword, filename):
-	ftp = ftplib.FTP(host = ftphost)
-	ftp.login(user = ftpuser, passwd = ftppassword)
-	ftp.cwd("Upload")
-	ftp.retrbinary('RETR %s' % filename, open(filename, 'wb').write)
+    ftp = ftplib.FTP(host = ftphost)
+    ftp.login(user = ftpuser, passwd = ftppassword)
+    ftp.cwd("Upload")
+    ftp.retrbinary('RETR %s' % filename, open("tmp/%s" % filename, 'wb').write)
     
 def correct_file(filename):
-	df = pd.read_csv(filename)
-	if "GeoLong" in df.columns:
-		df.loc[df["GeoLong"] > 0, "GeoLong"] *= -1
-	df["DummyField"] = 1
-	df.to_csv(filename, index = False)
+    filename = "tmp/%s" % filename
+    df = pd.read_csv(filename)
+    if "GeoLong" in df.columns:
+        df.loc[df["GeoLong"] > 0, "GeoLong"] *= -1
+    df["DummyField"] = 1
+    df.to_csv(filename, index = False)
 
 def update_arcgis(arcuser, arcpassword, arcfeatureid, filename):
-	gis = arcgis.GIS("http://lahub.maps.arcgis.com", arcuser, arcpassword)
-	gis_item = gis.content.get(arcfeatureid)
-	gis_layer_collection = arcgis.features.FeatureLayerCollection.fromitem(gis_item)
-	gis_layer_collection.manager.overwrite(filename)
+    filename = "tmp/%s" % filename
+    gis = arcgis.GIS("http://lahub.maps.arcgis.com", arcuser, arcpassword)
+    gis_item = gis.content.get(arcfeatureid)
+    gis_layer_collection = arcgis.features.FeatureLayerCollection.fromitem(gis_item)
+    gis_layer_collection.manager.overwrite(filename)
 
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
     "start_date": datetime(2019, 11, 14),
-    "email": ["ian.rose@lacity.org"],
+    "email": ["ian.rose@lacity.org", "hunter.owens@lacity.org", "brendan.bailey@lacity.org"],
     "email_on_failure": True,
     "email_on_retry": False,
     "retries": 1,
@@ -47,9 +50,10 @@ def update_rap_data(**kwargs):
     The actual python callable that Airflow schedules.
     """
     #Collecting data from FTP site
-    ftphost = kwargs.get("ftphost")
-    ftpuser = kwargs.get("ftpuser")
-    ftppassword = kwargs.get("ftppassword")
+    ftpconnection = BaseHook.get_connection("rap_ftp")
+    ftphost = ftpconnection.host
+    ftpuser = ftpconnection.login
+    ftppassword = ftpconnection.password
     filename = kwargs.get("filename")
     get_file(ftphost, ftpuser, ftppassword, filename)
     
@@ -57,8 +61,9 @@ def update_rap_data(**kwargs):
     correct_file(filename)
     
     #Updating ArcGis
-    arcuser = kwargs.get("arcuser")
-    arcpassword = kwargs.get("arcpassword")
+    arcconnection = BaseHook.get_connection("rap_ftp")
+    arcuser = arcconnection.login
+    arcpassword = arcconnection.password
     arcfeatureid = kwargs.get("arcfeatureid")
     update_arcgis(arcuser, arcpassword, arcfeatureid, filename)
 
@@ -68,12 +73,7 @@ t1 = PythonOperator(
     provide_context=True,
     python_callable=update_rap_data,
     op_kwargs={
-        "ftphost": "replace", #Replace with FTP Host Data
-        "ftpuser": "replace", #Replace with FTP User Data
-        "ftppassword": "replace", #Replace with FTP Password Data
         "filename": "ServiceRequestData.csv",
-        "arcuser":"replace", #Replace with AGOL User Data
-        "arcpassword": "replace", #Replace with AGOL Password Data
         "arcfeatureid": '96cd30870acd4c8f8999c734dfc5b480'
     },
     dag=dag,
@@ -85,13 +85,11 @@ t2 = PythonOperator(
     provide_context=True,
     python_callable=update_rap_data,
     op_kwargs={
-        "ftphost": "replace", #Replace with FTP Host Data
-        "ftpuser": "replace", #Replace with FTP User Data
-        "ftppassword": "replace", #Replace with FTP Password Data
         "filename": "raphrsrstats.csv",
-        "arcuser":"replace", #Replace with AGOL User Data
-        "arcpassword": "replace", #Replace with AGOL Password Data
         "arcfeatureid": '7fe1ef026bbe4bf9a07119eb134fb6cf'
     },
     dag=dag,
 )
+
+#Creating task order
+t1.set_downstream(t2)
