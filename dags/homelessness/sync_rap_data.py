1dcb6c53ce4b40a92bb9cea62fffc36135eea5bd
Ian Rose <ian.rose@lacity.org>
2019-11-18 10:47:46 -0800

    Update rap data.

--- dags/homelessness/sync_rap_data.py
+++ dags/homelessness/sync_rap_data.py
@@ -1,28 +1,32 @@
 """
 Pull data from RAP FTP and sync with ArcGIS Online
 """
-import pandas as pd
 import ftplib
-import arcgis
-
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.hooks.base_hook import BaseHook
 from datetime import datetime, timedelta
 
+import pandas as pd
+from airflow import DAG
+from airflow.hooks.base_hook import BaseHook
+from airflow.operators.python_operator import PythonOperator
+
+import arcgis
+
+
 def get_file(ftphost, ftpuser, ftppassword, filename):
-    ftp = ftplib.FTP(host = ftphost)
-    ftp.login(user = ftpuser, passwd = ftppassword)
+    ftp = ftplib.FTP(host=ftphost)
+    ftp.login(user=ftpuser, passwd=ftppassword)
     ftp.cwd("Upload")
-    ftp.retrbinary('RETR %s' % filename, open("/tmp/%s" % filename, 'wb').write)
-    
+    ftp.retrbinary("RETR %s" % filename, open("/tmp/%s" % filename, "wb").write)
+
+
 def correct_file(filename):
     filename = "/tmp/%s" % filename
     df = pd.read_csv(filename)
     if "GeoLong" in df.columns:
         df.loc[df["GeoLong"] > 0, "GeoLong"] *= -1
     df["DummyField"] = 1
-    df.to_csv(filename, index = False)
+    df.to_csv(filename, index=False)
+
 
 def update_arcgis(arcuser, arcpassword, arcfeatureid, filename):
     filename = "/tmp/%s" % filename
@@ -31,11 +35,16 @@ def update_arcgis(arcuser, arcpassword, arcfeatureid, filename):
     gis_layer_collection = arcgis.features.FeatureLayerCollection.fromitem(gis_item)
     gis_layer_collection.manager.overwrite(filename)
 
+
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
     "start_date": datetime(2019, 11, 14),
-    "email": ["ian.rose@lacity.org", "hunter.owens@lacity.org", "brendan.bailey@lacity.org"],
+    "email": [
+        "ian.rose@lacity.org",
+        "hunter.owens@lacity.org",
+        "brendan.bailey@lacity.org",
+    ],
     "email_on_failure": True,
     "email_on_retry": False,
     "retries": 1,
@@ -49,47 +58,48 @@ def update_rap_data(**kwargs):
     """
     The actual python callable that Airflow schedules.
     """
-    #Collecting data from FTP site
+    # Collecting data from FTP site
     ftpconnection = BaseHook.get_connection("rap_ftp")
     ftphost = ftpconnection.host
     ftpuser = ftpconnection.login
     ftppassword = ftpconnection.password
     filename = kwargs.get("filename")
     get_file(ftphost, ftpuser, ftppassword, filename)
-    
-    #Correcting issues with file
+
+    # Correcting issues with file
     correct_file(filename)
-    
-    #Updating ArcGis
+
+    # Updating ArcGis
     arcconnection = BaseHook.get_connection("arcgis")
     arcuser = arcconnection.login
     arcpassword = arcconnection.password
     arcfeatureid = kwargs.get("arcfeatureid")
     update_arcgis(arcuser, arcpassword, arcfeatureid, filename)
 
-#Sync ServiceRequestData.csv
+
+# Sync ServiceRequestData.csv
 t1 = PythonOperator(
     task_id="update_RAP_ServiceRequestData",
     provide_context=True,
     python_callable=update_rap_data,
     op_kwargs={
         "filename": "ServiceRequestData.csv",
-        "arcfeatureid": '96cd30870acd4c8f8999c734dfc5b480'
+        "arcfeatureid": "96cd30870acd4c8f8999c734dfc5b480",
     },
     dag=dag,
 )
 
-#Sync raphrsrstats.csv
+# Sync raphrsrstats.csv
 t2 = PythonOperator(
     task_id="update_raphrsrstats",
     provide_context=True,
     python_callable=update_rap_data,
     op_kwargs={
         "filename": "raphrsrstats.csv",
-        "arcfeatureid": '7fe1ef026bbe4bf9a07119eb134fb6cf'
+        "arcfeatureid": "7fe1ef026bbe4bf9a07119eb134fb6cf",
     },
     dag=dag,
 )
 
-#Creating task order
+# Creating task order
 t1.set_downstream(t2)
