ac3e74199c61b31a25b6f9a96edfa64a405bb673
Ian Rose <ian.rose@lacity.org>
2019-12-02 15:31:43 -0800

    Conditionally restart the scheduler and webserver if we are running on that node in CodeDeploy.

--- scripts/restart_app.sh
+++ scripts/restart_app.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
 sudo systemctl restart aqueduct-worker
+
+if [[ -z "${AQUEDUCT_SCHEDULER}" ]]; then
+  echo "Restarting scheduler and webserver"
+  sudo systemctl restart aqueduct-scheduler
+  sudo systemctl restart aqueduct-webserver
+fi
