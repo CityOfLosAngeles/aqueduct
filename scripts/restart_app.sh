ea4476410b43c37c8745029889a90047c910fd9e
Ian Rose <ian.rose@lacity.org>
2019-12-03 11:45:07 -0800

    Invert if statement.

--- scripts/restart_app.sh
+++ scripts/restart_app.sh
@@ -2,7 +2,7 @@
 
 sudo systemctl restart aqueduct-worker
 
-if [[ -z "${AQUEDUCT_SCHEDULER}" ]]; then
+if [[ ! -z "${AQUEDUCT_SCHEDULER}" ]]; then
   echo "Restarting scheduler and webserver"
   sudo systemctl restart aqueduct-scheduler
   sudo systemctl restart aqueduct-webserver
