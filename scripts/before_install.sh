7e3974b31c8e2ab1d80e9bdfc283813011df3fd0
Ian Rose <ian.rose@lacity.org>
2019-12-03 09:43:29 -0800

    Make pre-install a bit more robust.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,4 +2,7 @@
 
 # Remove the previous repository so we don't get conflicts
 # on trying to copy the new files during deployment.
-rm -rf /home/ec2-user/aqueduct
+if [ -d /home/ec2-user/aqueduct ]; then
+    rm -rf /home/ec2-user/aqueduct
+fi
+mkdir -vp /home/ec2-user/aqueduct
