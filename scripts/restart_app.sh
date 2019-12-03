eb0f25a8d974a9a9e31b42502767c0c4bbf1bc3f
Ian Rose <ian.rose@lacity.org>
2019-12-03 11:51:21 -0800

    Use CodeDeploy environment variables for restart services check.

--- scripts/restart_app.sh
+++ scripts/restart_app.sh
@@ -2,7 +2,7 @@
 
 sudo systemctl restart aqueduct-worker
 
-if [[ ! -z "${AQUEDUCT_SCHEDULER}" ]]; then
+if [ "$APPLICATION_NAME" == "Aqueduct-Scheduler" ]; then
   echo "Restarting scheduler and webserver"
   sudo systemctl restart aqueduct-scheduler
   sudo systemctl restart aqueduct-webserver
