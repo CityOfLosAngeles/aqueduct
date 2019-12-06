bfd1a26564c9eda4be61f2f1d6efa2e7fd286e7e
rmk0110 <ryan.kelly.ladot@gmail.com>
2019-12-06 11:28:09 -0800

    Removed aliases, removed ALTER FUNCTION statements, replaced absolute path, fixed indentation issue.

--- dags/transportation/dockless/v_trips.sql
+++ dags/transportation/dockless/v_trips.sql
@@ -5,30 +5,30 @@ DROP MATERIALIZED VIEW mds.v_trips CASCADE;
 CREATE MATERIALIZED VIEW mds.v_trips
 TABLESPACE pg_default
 AS
- SELECT mt.trips.provider_id,
-    mt.trips.trip_id,
-    mt.trips.provider_name,
-    mt.trips.device_id,
-    mt.trips.vehicle_id,
-    mt.trips.vehicle_type,
-    mt.trips.propulsion_type,
-    mt.trips.trip_duration,
-    mt.trips.trip_distance,
-    mt.trips.trip_distance::numeric * 0.000621371192 AS trip_distance_miles,
-    mt.trips.route as route_json,
-    mt.trips.start_time,
-    timezone('PST'::text, mt.trips.start_time) AS start_time_local,
-    mt.trips.end_time,
-    timezone('PST'::text, mt.trips.end_time) AS end_time_local,
-    mt.trips.parking_verification_url,
-    mt.trips.standard_cost,
-    mt.trips.actual_cost,
+ SELECT trips.provider_id,
+    trips.trip_id,
+    trips.provider_name,
+    trips.device_id,
+    trips.vehicle_id,
+    trips.vehicle_type,
+    trips.propulsion_type,
+    trips.trip_duration,
+    trips.trip_distance,
+    trips.trip_distance::numeric * 0.000621371192 AS trip_distance_miles,
+    trips.route as route_json,
+    trips.start_time,
+    timezone('PST'::text, trips.start_time) AS start_time_local,
+    trips.end_time,
+    timezone('PST'::text, trips.end_time) AS end_time_local,
+    trips.parking_verification_url,
+    trips.standard_cost,
+    trips.actual_cost,
 	trips_geoms.points as trip_geometry,
 	ST_StartPoint(trips_geoms.points) as start_point,
 	ST_EndPoint(trips_geoms.points) as end_point
-   FROM mds.trips mt INNER JOIN trips_geoms
+   FROM mds.trips INNER JOIN trips_geoms
    ON
-		mt.trips.trip_id = trips_geoms.trip_id
+		trips.trip_id = trips_geoms.trip_id
 WITH DATA;
 
 CREATE INDEX idx_trips_start_time ON mds.v_trips(start_time_local);
@@ -49,23 +49,23 @@ DROP MATERIALIZED VIEW mds.v_status_changes CASCADE;
 CREATE MATERIALIZED VIEW mds.v_status_changes
 TABLESPACE pg_default
 AS
- SELECT msc.status_changes.provider_id,
-    msc.status_changes.provider_name,
-    msc.status_changes.device_id,
-    msc.status_changes.vehicle_id,
-	msc.status_changes.vehicle_type,
-	msc.status_changes.propulsion_type,
-    msc.status_changes.event_type,
-    msc.status_changes.event_type_reason,
-    msc.status_changes.event_time,
-	msc.status_changes.battery_pct,
-	msc.status_changes.associated_trips,
-	msc.status_changes.id,
-    msc.status_changes.event_location as event_location_json,
-    timezone('PST'::text, msc.status_changes.event_time) AS event_time_local,
+ SELECT status_changes.provider_id,
+    status_changes.provider_name,
+    status_changes.device_id,
+    status_changes.vehicle_id,
+    status_changes.vehicle_type,
+    status_changes.propulsion_type,
+    status_changes.event_type,
+    status_changes.event_type_reason,
+    status_changes.event_time,
+    status_changes.battery_pct,
+    status_changes.associated_trips,
+    status_changes.id,
+    status_changes.event_location as event_location_json,
+    timezone('PST'::text, status_changes.event_time) AS event_time_local,
 	status_change_geoms.event_location_geom as event_location_geom
-   FROM mds.status_changes msc INNER JOIN status_change_geoms
-   ON msc.status_changes.id = status_change_geoms.status_change_id
+   FROM mds.status_changes INNER JOIN status_change_geoms
+   ON status_changes.id = status_change_geoms.status_change_id
 WITH DATA;
 
 CREATE INDEX idx_status_change_event_time ON mds.v_status_changes(event_time_local);
