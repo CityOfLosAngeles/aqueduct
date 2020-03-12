b31470c00ec05bb14c646335a9c779828e9652c6
hunterowens <owens.hunter@gmail.com>
2020-03-12 09:27:16 -0700

    add basic joins so dataset should be complete, not just cases

--- dags/public-health/jhu-to-esri.py
+++ dags/public-health/jhu-to-esri.py
@@ -7,6 +7,25 @@ DEATHS_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/c
 
 CASES_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
 
+RECOVERED_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+
+def parse_columns(df): 
+    """
+    quick helper function to parse columns into values 
+    uses for pd.melt
+    """
+
+    columns = list(df.columns)
+
+    id_vars, dates = [], []
+
+    for c in columns:
+        if c.endswith('20'):
+            dates.append(c)
+        else:
+            id_vars.append(c)
+    return id_vars, dates
+
 def load_jhu_to_esri(**kwargs): 
     """
     A single python ETL function. 
@@ -16,23 +35,36 @@ def load_jhu_to_esri(**kwargs):
 
     Ref: 
     """
-    pass
-
     cases = pd.read_csv(CASES_URL)
+    deaths = pd.read_csv(DEATHS_URL)
+    recovered = pd.read_csv(RECOVERED_URL)
+    # melt cases 
+    id_vars, dates = parse_columns(cases)
+    df = pd.melt(cases,
+                 id_vars=id_vars, 
+                 value_vars=dates, 
+                 value_name='number_of_cases', 
+                 var_name='date')
 
-    us_cases = cases[cases['Country/Region'] == 'US']   
+    # melt deaths
+    id_vars, dates = parse_columns(deaths) 
+    deaths_df = pd.melt(deaths, id_vars=id_vars, value_vars=dates, value_name='number_of_deaths')
 
-    columns = list(us_cases.columns)
+    # melt recovered 
 
-    id_vars, dates = [], []
+    id_vars, dates = parse_columns(deaths) 
+    recovered_df = pd.melt(recovered, id_vars=id_vars, value_vars=dates, value_name='number_of_recovered')
 
-    for c in columns:
-        if c.endswith('20'):
-            dates.append(c)
-        else:
-            id_vars.append(c)
+    # join
+    df['number_of_deaths'] = deaths_df.number_of_deaths
+    df['number_of_recovered'] = recovered_df.number_of_recovered
+    # make some simple asserts to assure that the data structures haven't changed and some old numbers 
+    # are still correct 
+    assert df.loc[(df['Province/State'] == 'Los Angeles, CA') & (df['date'] == '3/11/20')]['number_of_cases'].item() == 27
+    assert df.loc[(df['Province/State'] == 'Los Angeles, CA') & (df['date'] == '3/11/20')]['number_of_deaths'].item() == 1
+    assert df.loc[(df['Province/State'] == 'Shanghai') & (df['date'] == '3/11/20')]['number_of_recovered'].item() == 320
 
-    df = pd.melt(us_cases, id_vars=id_vars, value_vars=dates, value_name='number_of_cases')
+    df.loc[(df['Province/State'] == 'Los Angeles, CA') & (df['date'] == '3/11/20')]
     gdf = geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df.Longitude, df.Latitude))
     gdf.to_file('/tmp/jhu-dataset.shp')
 
@@ -48,7 +80,7 @@ def load_jhu_to_esri(**kwargs):
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2020-, 3, 1),
+    "start_date": datetime(2020, 3, 1),
     "email": [
         "ian.rose@lacity.org",
         "hunter.owens@lacity.org",
