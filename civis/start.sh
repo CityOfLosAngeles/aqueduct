7d517980cddeb081ca11f5d495ad04af44e2392a
Ian Rose <ian.r.rose@gmail.com>
2020-06-19 10:12:18 -0700

    Use REPO_PATH_DIR for specifying voila notebook.

--- civis/start.sh
+++ civis/start.sh
@@ -14,5 +14,5 @@ else
         --no-browser \
         --Voila.ip='*' \
         --Voila.tornado_settings=$'{"headers":{"Content-Security-Policy":"frame-ancestors platform.civisanalytics.com localhost:*; report-uri /api/security/csp-report" }}' \
-        /app
+        ${APP_DIR:-/app}/${REPO_PATH_DIR}
 fi
