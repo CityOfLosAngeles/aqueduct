b066f8c9324dba4c43a4074fc633793cb86d96eb
ian-r-rose <ian.r.rose@gmail.com>
2020-06-19 08:38:06 -0700

    Add aws-configure script to try to auto-detect and configure AWS credentials from the environment. Expects "AWS" to be in the credential names.

--- civis/start.sh
+++ civis/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Run the aws-configure script to try to detect AWS credentials from the environment
+aws-configure
+
+# Test if CIVIS_SERVICE_ID is defined. If so, we are likely in a deployed
+# service, so start voila. If not, we are likely in a Civis Jupyter notebook
+# context, so start that.
 if [[ -z $CIVIS_SERVICE_ID ]]; then
     civis-jupyter-notebooks-start
 else
