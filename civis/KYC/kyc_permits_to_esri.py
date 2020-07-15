d121110cb94524c417872e1e4871845010a5f0cf
prestinomills <prestonamills@gmail.com>
2020-07-15 15:48:16 -0700

    Updated permits script...again to include token and key

--- civis/KYC/kyc_permits_to_esri.py
+++ civis/KYC/kyc_permits_to_esri.py
@@ -15,12 +15,15 @@ from arcgis.features import FeatureLayerCollection
 
 lahub_user = os.environ["LAHUB_ACC_USERNAME"]
 lahub_pass = os.environ["LAHUB_ACC_PASSWORD"]
+socrata_user = os.environ["SOCRATA_ACC_PASSWORD"]
+socrata_pass = os.environ["SOCRATA_ACC_PASSWORD"]
+socrata_token = os.environ["SOCRATA_KEY_PASSWORD"]
 permit_layer = '48fca217dd5a410bbfd6ce0abcdd3a26'
 pwd = os.getcwd()
 OUTPUT_FILE = pwd + "/Building and Safety Permits, Last 6 Months.csv"
 
-def prep_permit_data(file):
-    client = Socrata("data.lacity.org", None)
+def prep_permit_data(file,token,user,pas):
+    client = Socrata("data.lacity.org", token, user, pas)
     df = pd.DataFrame(client.get('n9nq-vewq', limit=10000000))
     df2=df[((df.permit_sub_type == 'Apartment')|(df.permit_sub_type == 'Commercial'))&(
         (df.permit_type == 'Bldg-Addition')|(df.permit_type == 'Bldg-New')|(df.permit_type == 'Bldg-Demolition'))]
@@ -34,5 +37,5 @@ def update_geohub_layer(user, pw, layer, update_data):
 
 
 if __name__ == "__main__":
-    prep_permit_data(OUTPUT_FILE)
+    prep_permit_data(OUTPUT_FILE,socrata_token,socrata_user,socrata_pass)
     update_geohub_layer(lahub_user, lahub_pass, permit_layer, OUTPUT_FILE)
