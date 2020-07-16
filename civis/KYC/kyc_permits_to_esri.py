5fa0fee236f513c015c077b61e7bc5b7379461cb
prestinomills <prestonamills@gmail.com>
2020-07-15 22:44:22 -0700

    Updated permits script...again to see if client is finicky about designating user and pass

--- civis/KYC/kyc_permits_to_esri.py
+++ civis/KYC/kyc_permits_to_esri.py
@@ -16,15 +16,15 @@ import intake_civis
 
 lahub_user = os.environ["LAHUB_ACC_USERNAME"]
 lahub_pass = os.environ["LAHUB_ACC_PASSWORD"]
+socrata_token = 'LJ60SFL7ZqoC4IWosLhEmJV2a'
 socrata_user = os.environ["SOCRATA_ACC_PASSWORD"]
 socrata_pass = os.environ["SOCRATA_ACC_PASSWORD"]
-socrata_token = 'LJ60SFL7ZqoC4IWosLhEmJV2a'
 permit_layer = '48fca217dd5a410bbfd6ce0abcdd3a26'
 pwd = os.getcwd()
 OUTPUT_FILE = pwd + "/Building and Safety Permits, Last 6 Months.csv"
 
 def prep_permit_data(file,token,user,pas):
-    client = Socrata("data.lacity.org", token, user, pas)
+    client = Socrata("data.lacity.org", token, username=user, password=pas)
     df = pd.DataFrame(client.get('n9nq-vewq', limit=10000000))
     df2=df[((df.permit_sub_type == 'Apartment')|(df.permit_sub_type == 'Commercial'))&(
         (df.permit_type == 'Bldg-Addition')|(df.permit_type == 'Bldg-New')|(df.permit_type == 'Bldg-Demolition'))]
